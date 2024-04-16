ceeaa58f9f75317c8956b081026aab4fd5416da1
Uralegend <bcgraham917@gmail.com>
2024-04-16 17:12:39 -0400

    I realised the output look kind of sloppy so I fixed it up with spacing and added a message that said manufacturer details:.

--- Inheritance, Polymorphism, and Virtual Functions/Main.cpp
+++ Inheritance, Polymorphism, and Virtual Functions/Main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include "Car.h"
 #include "Truck.h"
-
+using namespace std;
 int main() {
     // Vehicle test
     std::string manufacturer;
@@ -13,18 +13,29 @@ int main() {
     std::cout << "Enter manufacturer and year built for Vehicle: ";
     std::cin >> manufacturer >> year;
     Vehicle vehicle(manufacturer, year);
+    std::cout << endl;
+    std::cout << "Manufacturer details:";
+    std::cout << endl;
     vehicle.displayInfo();
 
     // Car test
+    std::cout << endl;
     std::cout << "Enter manufacturer, year built and number of doors for Car: ";
     std::cin >> manufacturer >> year >> doors;
     Car car(manufacturer, year, doors);
+    std::cout << endl;
+    std::cout << "Manufacturer details:";
+    std::cout << endl;
     car.displayInfo();
 
     // Truck test
+    std::cout << endl;
     std::cout << "Enter manufacturer, year built and towing capacity for Truck: ";
     std::cin >> manufacturer >> year >> capacity;
     Truck truck(manufacturer, year, capacity);
+    std::cout << endl;
+    std::cout << "Manufacturer details:";
+    std::cout << endl;
     truck.displayInfo();
 
     return 0;
